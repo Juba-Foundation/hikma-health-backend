552eaf3a988c86e20eacc536c4f19fff76306cd6
3210jr <3210jr@gmail.com>
2022-11-27 13:05:56 -0700

    update to python 3.10

--- app/run-export.sh
+++ app/run-export.sh
@@ -2,4 +2,4 @@
 
 ./cloud_sql_proxy -instances=${DB_INSTANCE}=tcp:5432 -credential_file=${GOOGLE_APPLICATION_CREDENTIALS} &
 sleep 5
-python3.7 scripts/run_patient_data_export.py
+python3.10 scripts/run_patient_data_export.py
